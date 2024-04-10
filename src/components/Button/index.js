d27dc3a8531ce8214aff102e66a4be7e3fbe7ce7
lucasisnardi <lucasisnardi08@gmail.com>
2024-04-10 20:35:43 -0300

    atual

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -7,8 +7,8 @@ function Button({ children, ...rest }) {
   return <ContainerButton {...rest}>{children}</ContainerButton>
 }
 
-export default Button
-
 Button.propTypes = {
   children: PropTypes.string
 }
+
+export default Button
