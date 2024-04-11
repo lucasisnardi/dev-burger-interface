f355be528042e38a1a004585781158d638871eff
lucasisnardi <lucasisnardi08@gmail.com>
2024-04-11 19:21:22 -0300

    desktop

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom'
 
 import Login from './containers/Login'
+import Register from './containers/Register'
 import GlobalStyles from './styles/globalStyles'
 
 createRoot(document.getElementById('root')).render(
   <>
     <React.StrictMode>
-      <Login />
+      <Register />
       <GlobalStyles />
     </React.StrictMode>
   </>
