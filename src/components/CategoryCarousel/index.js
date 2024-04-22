5b2d5722a7e1952694c0c0ca4e2eafb169120deb
lucasisnardi <lucasisnardi08@gmail.com>
2024-04-22 19:25:29 -0300

    au

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Carousel from 'react-elastic-carousel'
 
 import Category from '../../assets/category.png'
 import api from '../../services/api'
 import { Container, CategoryImg } from './styles'
 
 function CategoryCarousel() {
+  const [categories, setCategories] = useState([])
+
   useEffect(() => {
     async function loadCategories() {
-      const response = await api.get('categories')
+      const { data } = await api.get('categories')
 
-      console.log(response)
+      setCategories(data)
     }
 
     loadCategories()
@@ -18,6 +21,16 @@ function CategoryCarousel() {
   return (
     <Container>
       <CategoryImg src={Category} alt="logo da categoria" />
+
+      <Carousel itemsToShow={4}>
+        {categories &&
+          categories.map(category => (
+            <div key={category.id}>
+              <img src={category.url} alt="foto da categoria" />
+              <button>{category.name}</button>
+            </div>
+          ))}
+      </Carousel>
     </Container>
   )
 }
