624482da9e45695c95b6c9ba2658a13cd149c3c0
lucasisnardi <lucasisnardi08@gmail.com>
2024-05-15 21:08:46 -0300

    tartarugas-ninjas

---
+++ src/containers/Admin/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { SideMenuAdmin } from '../../components'
+import ListProducts from './ListProducts'
+import Orders from './Orders'
+import { Container, ContainerItems } from './styles'
+
+export function Admin() {
+  return (
+    <Container>
+      <SideMenuAdmin />
+      <ContainerItems>
+        <Orders />
+        <ListProducts />
+      </ContainerItems>
+    </Container>
+  )
+}
